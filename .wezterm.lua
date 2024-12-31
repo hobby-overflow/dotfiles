477d0001eb59c14ffdd06124bf0ef844fc7bceb0
hobby-overflow <radio_old_boy_cq@yahoo.co.jp>
2024-12-31 20:53:13 +0900

    wezterm: reformat space to tabstop

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,18 +1,18 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 return {
-  color_scheme = 'Dark+',
-  use_ime = true,
-  font_size = 10.7,
-  font = wezterm.font 'UDEV Gothic 35NF',
-  warn_about_missing_glyphs=false,
-  hide_tab_bar_if_only_one_tab = true,
-  adjust_window_size_when_changing_font_size = false,
-  audible_bell = "Disabled",
-  window_padding = {
-      left = 0,
-      right = 0,
-      top = 0,
-      bottom = 0,
-  },
+	color_scheme = "Dark+",
+	use_ime = true,
+	font_size = 11.5, -- 10.7
+	font = wezterm.font("UDEV Gothic 35NF"),
+	warn_about_missing_glyphs = false,
+	hide_tab_bar_if_only_one_tab = true,
+	adjust_window_size_when_changing_font_size = false,
+	audible_bell = "Disabled",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
 }
